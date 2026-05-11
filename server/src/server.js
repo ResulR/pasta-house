3cdc4722af34562baa98dd98cc9f847423035da7
Debian <debian@vps-28bf7789.vps.ovh.net>
2026-05-11 01:22:13 +0000

    security: enforce 100kb json body limit

--- server/src/server.js
+++ server/src/server.js
@@ -50,7 +50,7 @@ app.use(cors({
 }));
 
 app.use("/api/public/stripe/webhook", express.raw({ type: "application/json" }));
-app.use(express.json());
+app.use(express.json({ limit: "100kb" }));
 app.use(cookieParser());
 app.use("/api/public", publicMenuRouter);
 app.use("/api/public", publicCheckoutRouter);
